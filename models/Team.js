546b6c142c848525e29154d28a92f249adaa61ef
veronica lim <veron_yan@yahoo.com.sg>
2020-10-16 10:50:09 +0800

    added auth, team, user in routes folder

--- models/Team.js
+++ models/Team.js
@@ -3,7 +3,6 @@ const Schema = mongoose.Schema;
 
 const teamSchema = new Schema({
   title: { type: String, required: true },
-  time: String,
   location: { type: String, required: true },
   homepage: { type: String, required: true },
   dateAdded: { type: Date, default: Date.now },
