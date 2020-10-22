03be3eae95a12a39c681643a917308d760befb26
veronica lim <veron_yan@yahoo.com.sg>
2020-10-22 10:51:37 +0800

    revised server.js

--- server.js
+++ server.js
@@ -1,74 +1,74 @@
-const express = require("express");
-// const connectDb = require('./database/connect');
-const path = require("path");
-const app = express();
-const PORT = process.env.PORT || 3001;
-const logger = require('morgan');
-// const cookieParser = require('cookie-parser');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const passport = require('passport');
-// const passportJWT = require('passport-jwt');
-const User = require('./models/User');
+require('module-alias/register');
+const CONFIG = require('@config/config');
 
-// Define middleware here
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-// Serve up static assets (usually on heroku)
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("frontend/build"));
+// Mongoose
+require('./db/mongoose');
+
+// Routes
+const authRouter = require('@routes/auth');
+const commonRouter = require('@routes/common');
+const usersRouter = require('@routes/users');
+const scoreRouter = require('@routes/score');
+const messageRouter = require('@routes/messages');
+
+// Others
+const express = require('express');
+const bodyParser = require('body-parser');
+const helmet = require('helmet');
+const compression = require('compression');
+const cors = require('cors');
+
+// Middlewares
+const auth = require('@middleware/auth');
+const errorHandler = require('@middleware/error-handler');
+
+// App
+const app = express();
+
+app.use(bodyParser.json({ limit: '5mb' }));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(helmet());
+app.use(compression());
+
+// CORS
+app.use(cors());
+
+// Routes
+// No auth required routes
+app.use('/auth', authRouter);
+app.use('/common', commonRouter);
+
+// Verify JWT and add user data to next requests
+app.use(auth);
+
+// Auth routes
+app.use('/users', usersRouter);
+app.use('/score', scoreRouter);
+app.use('/messages', messageRouter);
+
+// Handle errors only in development
+if (process.env.CURRENT_ENV === 'development') {
+  app.use(errorHandler);
+} else {
+  app.use((err, req, res, next) => {
+     console.error(err);
+     res.status(500).send('Server Error');
+  });
 }
 
-// Define API routes here
-const auth = require("./routes/auth");
-const team = require("./routes/team");
-const user = require("./routes/user");
-const connectDb = require("./database/connect");
+// Start the app
+const path = require("path");
+const PORT = process.env.PORT || 3001;
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-
-app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-//app.use(cookieParser(process.env.SESSION_SECRET));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors({
   origin: '*'
 }));
 
-// Passport + User
-passport.use(User.createStrategy());
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
-
-// Passport + JWT
-// passport.use(new passportJWT.Strategy(
-//   {
-//     jwtFromRequest: passportJWT.ExtractJwt.fromAuthHeader(),
-//     secretOrKey: process.env.TOKEN_SECRET
-//   },
-//   (payload, done) => {
-//     /*done(null, {
-//       _id: payload.sub
-//     })*/
-//     User.findById(payload.sub, done);
-//   }
-// ));
-
-// Express + Passport
-// app.use(passport.initialize());
-
-// defining routes
-app.use('/auth', auth);
-app.use('/team', team);
-app.use('/user', user);
-
-
-connectDb();
-
-
 app.listen(PORT, () => {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
 });
+
+module.exports = app;
